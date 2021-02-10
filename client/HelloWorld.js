b9128910add658d94c364faf7d4410d750777790
Maen22 <m3n.ibregeth@gmail.com>
2021-02-10 20:50:17 +0200

    initial commit

---
+++ client/HelloWorld.js
@@ -0,0 +1,13 @@
+import { hot } from "react-hot-loader/root";
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default hot(HelloWorld);
